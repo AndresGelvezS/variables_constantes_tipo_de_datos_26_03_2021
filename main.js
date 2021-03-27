a0b598950011a4a23fcda9d0019c83942dba178c
variables_constantes_tipo_de_datos_26_03_2021 <81273279+AndresGelvezS@users.noreply.github.com>
2021-03-27 10:10:01 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+
+
+//Variables
+
+let nombre;
+let apellido;
+const edad=37;
+nombre= "Andres";
+//nombre = prompt ("Ingrese su nombre") // Se usa prompt cuando se solicita que se digite los datos.
+apellido="Gelvez Suarez";
+console.log(nombre, apellido, edad);
+
+//Tipos de datos
+
+
+let Nombre = `Andres Gelvez Suarez`; //string
+let Edad=37; //numbre integer int;
+let altura= 1.60;// double float
+let profesor = false;// boleano
+let informacion =[Nombre, Edad, altura, profesor];// array: es una lista de datos 
+console.log(informacion);
+//console.log(informacion[2]);
+let vaso={
+    material : "porcelana",
+    diametro : 60,
+    lleno : false
+}; //objetos: descripcion o propiedad del objeto
+console.log(vaso);
+//console.log(vaso.diametro,vaso.diametro);
+//console.log (`${vaso.material} ${vaso.lleno}`);
+//let papa=' ' //Cuando es vacio, no hay nada. 
+//let papa= null //  es un dato nulo.
